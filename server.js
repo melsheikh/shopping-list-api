68b9964866bd43d398e0e43461ac5ae17278fe0b
Mostafa <m.elsheikh11791@gmail.com>
2016-09-29 07:20:21 +0200

    fixed yesterday's issues

--- server.js
+++ server.js
@@ -12,25 +12,31 @@ var Storage = {
     },
     remove: function(id) {
         var index;
-        for(var i = this.items.length; i >= 0; i--) {
-          if (this.items[i] === id) {
-            index = i - 1;
+        for(var i = this.items.length - 1 ; i >= 0; i--) { //last index is length - 1 to avoid array access exception
+          if (this.items[i].id == id) { // comparing IDs is better than comparing full objects
+            index = i;
+            break;
           }
-            else{
-                if(this.items[i] <= 2)
-                index = i;
-            }
         }
         this.items.splice(index, 1);
         return id;
     },
     edit: function(id, item) {
         var item = {
-            name: item, 
+            name: item,
             id:id
         };
-        var oldItem = Storage.item;
-        this.items.splice(id - 1, 1, item);
+        var index;
+        for(var i = this.items.length - 1 ; i >= 0; i--) {
+          if (this.items[i].id == id) { // use of the === operator is dangerous here,
+            // the id passed in coming from request.params is a string and is compared to an
+            // integer inside the this.items array which returns false even if they are the
+            // same IDs. This messed things up. Same applied to the remove function.
+            index = i;
+            break;
+          }
+        }
+        this.items.splice(index, 1, item);
     }
 };
 var createStorage = function() {
@@ -53,7 +59,7 @@ app.get('/items', function(request, response) {
     response.json(storage.items);
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP);
+app.listen(3000, process.env.IP);
 
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
@@ -70,16 +76,18 @@ app.post('/items', jsonParser, function(request, response) {
 
 app.delete('/items/:id', jsonParser, function(request, response) {
   var id = request.params.id;
-  var deleted = storage.remove(storage.items[id])
+  // id here is a string, which when passed in to the remove function is compared to an
+  // integer stored inside the items list. Using the === operator is dangerous
+  // as it always returns false due to the difference in data types even if IDs do match.
+  // I changed this to use == instead.
+  var deleted = storage.remove(id) //pass the id for comparison
   return response.status(200).json(deleted);
 });
 
 app.put('/items/:id', jsonParser, function(request, response) {
   var id = request.params.id
-  console.log(request.body.name);
+  // Same applies as above.
   var newItem = request.body.name;
   storage.edit(id, newItem);
   return response.status(200).json(newItem);
 });
-
-
